8f54f7715e4fb38ff5a7faf73e6e730afcdd6cc2
Kevin Zhang <zjuerkzhang@hotmail.com>
2019-01-15 12:37:09 +0800

    Improve work.sh

--- work.sh
+++ work.sh
@@ -2,11 +2,22 @@
 export LANG="zh_CN.UTF-8"
 
 cd /root/RssFullTrans
-cp /usr/share/nginx/html/rss/RSS_*.xml ./
+
+if [ -d "/var/www/rss" ]
+then
+    rss_dir=/var/www/rss
+elif [ -d "/usr/shared/nginx/html/rss" ]
+then
+    rss_dir=/usr/shared/nginx/html/rss
+else
+    rss_dir=/root/RssFullTrans
+fi
+
+cp $rss_dir/RSS_*.xml ./
 python RssFullTrans.py
 
 ls |grep RSS_ 
 if [ $? -eq 0 ]
 then
-	mv RSS_*.xml /usr/share/nginx/html/rss/
+	mv RSS_*.xml $rss_dir/ 
 fi
